2f7503c8dd6c85ff1ffe9d58b42bcfe7781adc64
KienenManeiro <jowk010@gmail.com>
2024-10-18 23:50:06 -0300

    feat: remoção do uso da interface do Swagger

--- Program.cs
+++ Program.cs
@@ -16,27 +16,32 @@ builder.Services.AddCors(options =>
     });
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
+/*
+//swagger as debbuging device
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen();*/
 
 builder.Services.AddTransient<ITarefaRepository, TarefaRepository>();
 
-var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+// Configure the HTTP request pipeline. 
+//swagger as debbuging device
+/*if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+*/
 
+var app = builder.Build();
 app.UseHttpsRedirection();
 
 app.UseRouting();
 app.UseCors("AllowAllOrigins");//DB
+
 
 app.UseAuthorization();
 
